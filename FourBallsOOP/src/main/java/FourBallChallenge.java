24355290870cf88e47f997ea705e33933d94d2c8
Md Mubasir <54818531+mdmub0587@users.noreply.github.com>
2021-02-24 22:33:14 +0530

    Refactored Variables

--- FourBallsOOP/src/main/java/FourBallChallenge.java
+++ FourBallsOOP/src/main/java/FourBallChallenge.java
@@ -3,11 +3,14 @@ public class FourBallChallenge extends PApplet{
 
     public static final int WIDTH = 750;
     public static final int HEIGHT = 500;
+    public static final int DIAMETER = 10;
+    public static final float COMMON_HEIGHT_FACTOR = 1 / 5f;
+    public static final int COMMON_SPEED_FACTOR = 1;
 
-    Ball ball1 = new Ball(10, 1 / 5f, 1);
-    Ball ball2 = new Ball(10, 2 / 5f, 2);
-    Ball ball3 = new Ball(10, 3 / 5f, 3);
-    Ball ball4 = new Ball(10, 4 / 5f, 4);
+    Ball ball1 = new Ball(DIAMETER, 1 * COMMON_HEIGHT_FACTOR, 1 * COMMON_SPEED_FACTOR);
+    Ball ball2 = new Ball(DIAMETER, 2 * COMMON_HEIGHT_FACTOR, 2 * COMMON_SPEED_FACTOR);
+    Ball ball3 = new Ball(DIAMETER, 3 * COMMON_HEIGHT_FACTOR, 3 * COMMON_SPEED_FACTOR);
+    Ball ball4 = new Ball(DIAMETER, 4 * COMMON_HEIGHT_FACTOR, 4 * COMMON_SPEED_FACTOR);
 
     int positionX = 0;
 
