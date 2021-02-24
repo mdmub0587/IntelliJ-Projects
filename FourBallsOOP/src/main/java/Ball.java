889a10c4a1dde4b65426a1050d5659a0e2aa8066
Md Mubasir <54818531+mdmub0587@users.noreply.github.com>
2021-02-24 19:54:53 +0530

    Set Ball's attribute as final

--- FourBallsOOP/src/main/java/Ball.java
+++ FourBallsOOP/src/main/java/Ball.java
@@ -1,7 +1,7 @@
 public class Ball {
     public final int diameter;
     public final float heightFactor;
-    public int speedFactor;
+    public final int speedFactor;
     public Ball(int diameter, float heightFactor,int speedFactor) {
         this.diameter = diameter;
         this.heightFactor = heightFactor;
